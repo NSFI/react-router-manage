e6b3d47931a05cf1dbb120d740c8bad19f49e100
onshinpei <244221001@qq.com>
2022-09-14 23:01:15 +0800

    docs: actions demo

--- examples/actions/src/main.tsx
+++ examples/actions/src/main.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-
-import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement!);
+
+root.render(
+  // <StrictMode>
+    <App />
+  // </StrictMode>
 );
